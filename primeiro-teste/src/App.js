61a59a869c27451a31d246b9995a4a76890e4895
victorbessemer2 <victor.santos893@etec.sp.gov.br>
2025-08-01 18:13:21 -0300

    envio inicial

---
+++ primeiro-teste/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Meu app react</h1>
+    <p>wake up its first the of month!!</p>
+    </div>
+  );
+}
+
+export default App;
